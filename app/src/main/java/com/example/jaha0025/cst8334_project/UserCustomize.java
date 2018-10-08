128ba73f3a8900773b5c97154f6dee4cacd8ea05
Jermal-Lee <30882921+Jermal-Lee@users.noreply.github.com>
2018-10-08 18:57:22 -0400

    Created avatar screen with graphics.  Added back button to return to UserProfile.

---
+++ app/src/main/java/com/example/jaha0025/cst8334_project/UserCustomize.java
@@ -0,0 +1,28 @@
+package com.example.jaha0025.cst8334_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class UserCustomize extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_customize);
+
+        ImageView back = findViewById(R.id.close);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserCustomize.this, UserProfile.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
