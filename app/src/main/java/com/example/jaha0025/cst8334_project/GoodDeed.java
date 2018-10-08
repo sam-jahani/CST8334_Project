5635b2f834ae8a5a5fdc33c29c6f324820279add
phillywolf22 <tharakan22@gmail.com>
2018-10-08 14:34:51 -0400

    Branch Philip adding to Sam1 branch

--- app/src/main/java/com/example/jaha0025/cst8334_project/GoodDeed.java
+++ app/src/main/java/com/example/jaha0025/cst8334_project/GoodDeed.java
@@ -36,15 +36,23 @@ Next Screen
 
  */
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GoodDeed extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 50;
+    ImageView imageViewUpload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +75,34 @@ public class GoodDeed extends AppCompatActivity {
 
         textView3.setTextSize(20);
 
+        imageViewUpload= findViewById(R.id.imageView2);
+
+        imageViewUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+
+
 
         //Button button = findViewById(R.id.button);
         //button.setBackground(getDrawable(@drawable.bee.png));
         //button.setText();
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageViewUpload.setImageBitmap(imageBitmap);
+        }
     }
 }
