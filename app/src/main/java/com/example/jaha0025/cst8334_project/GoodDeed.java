d84860eb2a3a4123c7d82271a35c1dee35cc2034
Sandro <smaculan@gmail.com>
2018-10-10 16:20:10 -0400

    Db connection commit.

--- app/src/main/java/com/example/jaha0025/cst8334_project/GoodDeed.java
+++ app/src/main/java/com/example/jaha0025/cst8334_project/GoodDeed.java
@@ -36,7 +36,9 @@ Next Screen
 
  */
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.provider.MediaStore;
@@ -48,33 +50,54 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class GoodDeed extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 50;
     ImageView imageViewUpload;
-
+    TextView textView, textView2, textView3;
+    public Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good_deed);
+        //sanmac
+        ArrayList<ActOfKindness> acts = ActOfKindness.insertActs(this);
+        ActDbAdapter adapter = new ActDbAdapter(this);
+        adapter.open();
+        for(ActOfKindness act : acts) {
+            ContentValues newValues = new ContentValues();
+            newValues.put(ActDbAdapter.TITLE, act.aTitle);
+            newValues.put(ActDbAdapter.DESCRIPTION, act.aDescription);
+            newValues.put(ActDbAdapter.QUESTION, act.aQuestion);
+            adapter.insertAct(newValues);
+        }
+        cursor = adapter.getActs();
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        ActOfKindness act;
+        if(cursor.moveToFirst()){
+            act = ActDbAdapter.getActFromCursor(cursor);
 
-//        ImageView imageView = find
+            textView.setText(act.aDescription);
+            textView2.setText(act.aQuestion);
+            textView3.setText(act.aTitle);
+            //textView.setText("Make a “Welcome to the Neighbourhood” basket for a family who has just moved in. Include some cookies or muffins. Make a list of fun things to do in the neighbourhood – you could include your favourite ice cream store, movie store, the library, your favourite parks, etc. You could also include a hand drawn map. Deliver the basket to the new family’s home. Bring your whole family to deliver the basket to your new neighbours - this way everyone can be introduced.");
+            //  textView.setFontFeatureSettings("gothicfontbold.tff");
+            //textView.setBackgroundColor(Color.YELLOW);
 
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("Make a “Welcome to the Neighbourhood” basket for a family who has just moved in. Include some cookies or muffins. Make a list of fun things to do in the neighbourhood – you could include your favourite ice cream store, movie store, the library, your favourite parks, etc. You could also include a hand drawn map. Deliver the basket to the new family’s home. Bring your whole family to deliver the basket to your new neighbours - this way everyone can be introduced.");
-      //  textView.setFontFeatureSettings("gothicfontbold.tff");
-        //textView.setBackgroundColor(Color.YELLOW);
-        TextView textView2 = findViewById(R.id.textView2);
-        textView2.setText("How did this make the new family feel? Did you learn anything interesting about the family? Where did they move from? How old are the kids? How did it make you feel to give this basket to the new family?");
-        textView2.setTextColor(Color.BLACK);
+            //textView2.setText("How did this make the new family feel? Did you learn anything interesting about the family? Where did they move from? How old are the kids? How did it make you feel to give this basket to the new family?");
+            textView2.setTextColor(Color.BLACK);
 
-        TextView textView3 = findViewById(R.id.textView3);
-        textView3.setText("Welcome a Neighbour");
-        textView3.setTextColor(Color.BLACK);
 
-        textView3.setTextSize(20);
+            //textView3.setText("Welcome a Neighbour");
+            textView3.setTextColor(Color.BLACK);
 
+            textView3.setTextSize(20);
+        }
         imageViewUpload= findViewById(R.id.imageView2);
 
         imageViewUpload.setOnClickListener(new View.OnClickListener() {
