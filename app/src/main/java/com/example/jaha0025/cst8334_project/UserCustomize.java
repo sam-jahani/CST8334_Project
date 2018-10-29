66bf5eb39fbdf8e357bb230ba99c96210db5d54e
Jermal-Lee <30882921+Jermal-Lee@users.noreply.github.com>
2018-10-29 18:09:24 -0400

    added bg music

--- app/src/main/java/com/example/jaha0025/cst8334_project/UserCustomize.java
+++ app/src/main/java/com/example/jaha0025/cst8334_project/UserCustomize.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.media.MediaPlayer;
 
 public class UserCustomize extends AppCompatActivity {
+
+    MediaPlayer testSound1 = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,34 @@ public class UserCustomize extends AppCompatActivity {
             }
         });
 
+        Play();
+
 
     }
+
+    public void Play() {
+        if (testSound1 == null) {
+
+            // instaniate the MediaPlayer object
+            testSound1 = MediaPlayer.create(this, R.raw.test_sound);
+            //starting the recording
+            testSound1.start();
+            // when the recording is finished release the MediaPlayer
+            testSound1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    //this code actually releases the player
+                    //testSound.release();
+                    testSound1.start();
+
+                }
+            });
+        }
+    }
+
+    public void onPause(){
+        testSound1.release();
+        super.onPause();
+    }
 }
+
